e79db8f97e1c9dd1a918773b8fd78cbd03a544b3
Suyash9698 <52395988+Suyash9698@users.noreply.github.com>
2023-09-06 21:09:43 +0530

    Delete 2023202017_q2_B.sql

--- 2023202017_q2_B.sql
+++
@@ -1,6 +0,0 @@
-select SSDLab.books.genre,count(SSDLab.issued_users.user_id) as total_user_count
-from SSDLab.authors INNER JOIN SSDLab.books 
-INNER JOIN SSDLab.issued_users
-ON SSDLab.issued_users.book_id=SSDLab.books.book_id and
-SSDLab.authors.author_id=SSDLab.books.author_id
-group by SSDLab.books.genre;
