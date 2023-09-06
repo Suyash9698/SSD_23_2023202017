a53a8f993d979ace5e8b4be46443c6b650cfd796
Suyash9698 <52395988+Suyash9698@users.noreply.github.com>
2023-09-06 21:08:43 +0530

    Delete 2023202017_q1_B.sql

--- 2023202017_q1_B.sql
+++
@@ -1,8 +0,0 @@
-alter table SSDLab.menu add food_type varchar(100);
-update SSDLab.menu
-set food_type = 'veg'
-where dish_name like '%Paneer%';
-update SSDLab.menu
-set food_type='non-veg'
-where dish_name like '%Chicken%';
-select * from SSDLab.menu;
